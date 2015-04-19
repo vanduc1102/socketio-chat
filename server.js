6dd1b9a29bcd1f2ac4b3932956dd1e9b81a07fc8
Duc NGUYEN <vanducld@gmail.com>
2015-04-19 23:09:15 +0700

    ready to upload to heroku

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var app = express();
 var http = require('http').Server(app);
 var compression = require('compression');
 
+var httpPort = process.env.PORT || 3000;
 var oneDay = 86400000;
 
 app.use(compression());
 
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(httpPort, function(){
+  console.log('listening on *:'+httpPort);
 });
