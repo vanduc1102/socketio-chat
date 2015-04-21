e3194cd7eedad8c1c29e4445978aa7b9d451e0bd
Duc NGUYEN <vanducld@gmail.com>
2015-04-22 00:17:26 +0700

    #1 add handle socketIo for test

--- public/js/welcome-ctrl.js
+++ public/js/welcome-ctrl.js
@@ -1,19 +1,38 @@
 myApp.controller('WelcomeCtrl', function ($scope, $location,$http, $window) {
-  $scope.message = '';
-  
-  $scope.logoutButtonClick = function(){
-	delete $window.sessionStorage.token;
-    $location.path("/");
-  }
-  
-  $http
-      .get('/api/welcome')
-      .success(function (data, status, headers, config) {
-		console.log("data : ",data);
-        $scope.message = data["message"];
-      })
-      .error(function (data, status, headers, config) {
-        delete $window.sessionStorage.token;
-        $scope.message = 'Error: Invalid user or password';
-      });
+	$scope.message = '';
+    var socket;
+	$scope.logoutButtonClick = function(){
+		delete $window.sessionStorage.token;
+		$location.path("/");
+	}
+	
+	function connect () {
+		socket = io.connect($window.sessionStorage.token ? ('?token=' + $window.sessionStorage.token) : '', {
+			'forceNew': true
+		});
+		socket.on('pong', function () {
+			console.log('- pong');
+		}).on('time', function (data) {
+			console.log('- broadcast: ' + data);
+		}).on('authenticated', function () {
+			console.log('- authenticated');
+		}).on('disconnect', function () {
+			console.log('- disconnected');
+		}).on("error", function(error) {
+			if (error.type == "UnauthorizedError" || error.code == "invalid_token") {
+				// redirect user to login page perhaps?
+				console.log("User's token has expired");
+			}
+		});
+	}
+	connect(); //connect now, it will drop
+		
+	$http.get('/api/welcome')
+	.success(function (data, status, headers, config) {
+		$scope.message = data["message"];
+    })
+	.error(function (data, status, headers, config) {
+		delete $window.sessionStorage.token;
+		$scope.message = 'Error: Invalid user or password';
+    });
 });
